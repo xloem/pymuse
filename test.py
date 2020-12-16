9162583b192339f79b52709123e06562f3f73c16
0xloem <0xloem@gmail.com>
2020-12-16 14:17:08 -0500

    1

--- test.py
+++ test.py
@@ -9,41 +9,50 @@ from gi.repository import GLib
 import signal
 from threading import Thread
 
-# int gattlib_string_to_uuid(const char *str, size_t size, uuid_t *uuid);
-gattlib_string_to_uuid = gattlib.gattlib_string_to_uuid
-gattlib_string_to_uuid.argtypes = [c_char_p, c_size_t , POINTER(GattlibUuid)]
+MUSE_MAC_PREFIX = '00:55:DA:'
+PRIMARY_SERVICE = '0000fe8d-0000-1000-8000-00805f9b34fb'
+CHARACTERISTIC_UUID_TEMPLATE = '273e00{}-4c4d-454d-96be-f03bac821358'
 
-SUCCESS = 0
-INVALID_PARAMETER = 1
-NOT_FOUND = 2
-OUT_OF_MEMORY = 3
-NOT_SUPPORTED = 4
-DEVICE_ERROR = 5
-ERROR_DBUS = 6
-ERROR_BLUEZ = 7
-ERROR_INTERNAL = 8
+GATT_CHARACTERISTIC_IDS = {
+    # i'm not sure these names have the right numbers
+    # the presets are associated with the numbers, not the names, on my Muse S
+    'SERIAL': 1,
+    # packets start with a 16-bit sequence number
+    # eeg signals are all 12-bit numbers concatenated together
+    'SIGNAL_AUX_LEFT': 2,  #                                              p63
+    'SIGNAL_TP9': 3,       # p20, p21, p22, p23, p50, p51, p52, p60, p61, p63
+    'SIGNAL_FP1': 4,       # p20, p21, p22, p23, p50, p51, p52, p60, p61, p63
+    'SIGNAL_FP2': 5,       # p20, p21, p22, p23, p50, p51, p52, p60, p61, p63
+    'SIGNAL_TP10': 6,      # p20, p21, p22, p23, p50, p51, p52, p60, p61, p63
+    'SIGNAL_AUX_RIGHT': 7, # p20            p23  p50            p60       p63
+    'DRL_REF': 8,          # p20  p21  p22  p23  p50  p51  p52  p60  p61  p63
+    # imu data is sent as 16-bit numbers, 3 3-axis vectors at a time
+    'GYRO': 9,             # p20  p21            p50  p51       p60  p61  p63
+    'ACCELEROMETER': 0xa,  # p20  p21            p50  p51       p60  p61  p63
+    # battery packet format is uncertain
+    'BATTERY': 0xb,        # p20  p21  p22  p23  p50  p51  p52  p60  p61  p63
+    'MAGNETOMETER': 0xc,   #                        not on muse S
+    'PRESSURE': 0xd,       #                        not on muse S
+    'ULTRA_VIOLET': 0xe,   #                        not on muse S
+    # PPG appears to be 24bit intensity data.
+    'PPG_AMBIENT': 0xf,    #                     p50  p51  p52  p60  p61  p63
+    'PPG_IR': 0x10,        #                     p50  p51  p52  p60  p61  p63
+    'PPG_RED': 0x11,       #                     p50  p51  p52  p60  p61  p63
+    # thermistor has 12-bit format, like the eeg signals
+    # it looks like lower numbers indicate warmer temperature
+    'THERMISTOR': 0x12,    # p20       p22       p50            p60       p63
+}
+GATT_CHARACTERISTIC_UUIDS = {    
+    name: CHARACTERISTIC_UUID_TEMPLATE.format('%02x' % id)
+    for name, id in GATT_CHARACTERISTIC_IDS.items()
+}
 
-def usage():
-    print(sys.argv[0], '<device_address> <notification_characteristic_uuid> [<write_characteristic_uuid> <write_characteristic_hex_data> ...]')
 
 def main():
-    notify_uuid = UUID(sys.argv[2])
-    if len(sys.argv) > 3:
-        write_uuid = UUID(sys.argv[3])
-    argv = [bytes(arg, 'utf-8') for arg in sys.argv]
-    if len(argv) < 3:
-        return usage()
-        
-
-    #notify_uuid = GattlibUuid()
-    #write_uuid = GattlibUuid()
-
-    #if gattlib_string_to_uuid(argv[2], len(argv[2]) + 1, byref(notify_uuid)) < 0:
-    #    return usage()
-
-    #if len(argv) > 3:
-    #    if gattlib_string_to_uuid(argv[3], len(argv[3]) + 1, byref(write_uuid)) < 0:
-    #        return usage()
+    argv=[sys.argv[0], MUSE_MAC_PREFIX + 'BB:1C:52', GATT_CHARACTERISTIC_UUIDS['SERIAL'], GATT_CHARACTERISTIC_UUIDS['SERIAL'], '0376310a']
+    notify_uuid = UUID(argv[2])
+    if len(argv) > 3:
+        write_uuid = UUID(argv[3])
 
     loop = GLib.MainLoop()
     thread = Thread(target=loop.run)
@@ -72,7 +81,7 @@ def main():
         notify_characteristic.notification_start()
     
         for arg in argv[4:]:
-            data = bytes.fromhex(arg.decode('utf-8'))
+            data = bytes.fromhex(arg)
             write_characteristic.write(data)
     
         def on_user_abort(signum, frame):
