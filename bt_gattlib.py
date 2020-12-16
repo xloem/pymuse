66be2dac93a9865a466e34441d4156ff008f1db8
0xloem <0xloem@gmail.com>
2020-12-16 04:08:17 -0500

    small reliability changes during dev

--- bt_gattlib.py
+++ bt_gattlib.py
@@ -13,19 +13,19 @@ class Interface:
         self._adapter = gattlib.adapter.Adapter()
         self._devices = {}
         self._addrs = []
+        self._adapter.open()
 
     def start_scanning(self, callback = None):
         self._devices = []
+        self._scanning = True
 
         # 2020-12
         # this would be an easy patch to gattlib, similar to the characteristic enumeration code
         print('')
         print('Warning: gattlib has no way to enumerate devices that are already connected via bluetooth.')
         print('')
-
-        self._adapter.open()
+        
         sem = Semaphore(0)
-        cont = True
         def on_scan(device, userdata):
             addr = str(device.id)
             name = str(device)
@@ -33,21 +33,30 @@ class Interface:
             print('device {} {}'.format(addr,name))
             sem.release()
         def pump():
-            sem.acquire()
-            while cont:
-                print('sem {}', self._devices[-1])
+            while True:
+                if sem.acquire(timeout=0.5):
+                    print('sem {}'.format(self._devices[-1][0]))
+                if not self._scanning:
+                    break
                 callback(self._devices)
-                sem.acquire()
         thread = Thread(target=pump)
         thread.start()
                         
-        self._adapter.scan_enable(on_scan, 0)
-        cont = False
+        try:
+            # it looks like CTRL-C/SIGINT doesn't interrupt the process during gattlib calls
+            # probably not hard to work around that and make it work, maybe by installing a signal handler
+            # is probably also a bug in gattlib that would be easy to fix
+            self._adapter.scan_enable(on_scan, 0)
+        except gattlib.exception.DBusError as e:
+            e.args.append('Is Bluetooth turned on?')
+            raise e
+
         sem.release()
         thread.join()
     
     def stop_scanning(self):
         print('stopping scanning')
+        self._scanning = False
         self._adapter.scan_disable()
 
     def near_addresses(self):
@@ -67,9 +76,11 @@ class LEDevice:
         self._device.discover()
         print('discovered')
     def __del__(self):
-        print('DISCONNECT')
-        self._device.disconnect()
-        atexit.unregister(self.__del__)
+        if self._device:
+            device = self._device
+            self._device = None
+            print('DISCONNECT')
+            device.disconnect()
     def info(self):
         return {
             'address': str(self._device.id)
@@ -84,6 +95,8 @@ class Characteristic:
         self._uuid = uuid
         self._characteristic = None
         self._subscribees = set()
+        for key, value in self._device._device.characteristics.items():
+            print(str(key), str(value))
         for characteristic in self._device._device.characteristics.values():
             if str(characteristic.uuid) == uuid:
                 if self._characteristic is not None:
@@ -91,19 +104,24 @@ class Characteristic:
                 self._characteristic = characteristic
         if self._characteristic is None:
             raise ValueError('no such characteristic on device')
+        print('uuid', self._characteristic.uuid)
         self._characteristic.register_notification(self._notification_registrant)
     def _notification_registrant(self, data, user_data):
         # current interface is for ONE subscriber at once; this is just to ease changes if they happen
+        print('notify!')
         for subscribee in self._subscribees:
             subscribee(data)
     def subscribe(self, callback):
+        print('subscribe!')
         self._subscribees.add(callback)
         self._characteristic.notification_start()  
     def unsubscribe():
         self._subscribees.clear()
         self._characteristic.notification_stop()
     def write(self, data : bytes):
-        self._characteristic.write(data)
+        print('write!', data)
+        for byte in data:
+            self._characteristic.write(bytes(byte,))
 
 _interface = Interface()
 
